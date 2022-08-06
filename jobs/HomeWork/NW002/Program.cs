f3c2b1fd0e51a2db0d64fdfb21c1df37b222b243
«Anton» <hranitel-liks@mail.ru>
2022-08-07 00:36:24 +0700

    add NW002_NW003

---
+++ jobs/HomeWork/NW002/Program.cs
@@ -0,0 +1,40 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+
+string m = "Понедельник";
+string t = "Вторник";
+string w = "Среда";
+string th = "Четверг";
+string f = "Пятница";
+string sa = "Суббота";
+string su = "Воскресенье";
+
+if (a == 1)
+{
+   Console.WriteLine(m);
+}
+if (a == 2)
+{
+   Console.WriteLine(t);
+}
+if (a == 3)
+{
+   Console.WriteLine(w);
+}
+if (a == 4)
+{
+   Console.WriteLine(th);
+}
+if (a == 5)
+{
+   Console.WriteLine(f);
+}
+if (a == 6)
+{
+   Console.WriteLine(sa);
+}
+if (a == 7)
+{
+   Console.WriteLine(su);
+}
--- jobs/NW002/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// По заданному номеру дня недели вывести его название
-
-Console.WriteLine("Введите номер: ");
-string username = Console.ReadLine();
-
-numder1 = Man;
-Console.WriteLine(unumder1);
