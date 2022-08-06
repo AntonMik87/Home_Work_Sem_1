005f8d1ca0011de27a4efa92513c4f941ae8286b
«Anton» <hranitel-liks@mail.ru>
2022-08-07 01:18:35 +0700

    add HM004

--- jobs/HomeWork/HW004/Program.cs
+++ jobs/HomeWork/HW004/Program.cs
@@ -1,15 +1,4 @@
 ﻿// Выяснить является ли число чётным
-//int x;
-//x = Console.Read();
-//if ((x % 2) == 0)
-{
-    //Console.WriteLine("четное");
-}
-//else
-{
-    //Console.WriteLine("нечетное");
-}
-// Выяснить является ли число чётным
 int x;
 x = Console.Read();
 if(x%2==0)
