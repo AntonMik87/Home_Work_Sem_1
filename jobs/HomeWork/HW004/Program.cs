2877b9d6b09439a7cb43af031a83d8158c32eba9
«Anton» <hranitel-liks@mail.ru>
2022-08-07 01:17:53 +0700

    add HM004

--- jobs/HomeWork/HW004/Program.cs
+++ jobs/HomeWork/HW004/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить является ли число чётным
+//int x;
+//x = Console.Read();
+//if ((x % 2) == 0)
+{
+    //Console.WriteLine("четное");
+}
+//else
+{
+    //Console.WriteLine("нечетное");
+}
+// Выяснить является ли число чётным
+int x;
+x = Console.Read();
+if(x%2==0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
