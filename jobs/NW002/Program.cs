61b1809bf2cb2f5e4212f9ab6c199dbec15d8331
«Anton» <hranitel-liks@mail.ru>
2022-08-06 12:13:58 +0700

    add MW002_NW003

---
+++ jobs/NW002/Program.cs
@@ -0,0 +1,7 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер: ");
+string username = Console.ReadLine();
+
+numder1 = Man;
+Console.WriteLine(unumder1);
