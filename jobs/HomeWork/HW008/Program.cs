4f8e79885b019a66ca5e03dc871ea2cf1c46f370
«Anton» <hranitel-liks@mail.ru>
2022-08-07 21:54:06 +0700

    add HW008

---
+++ jobs/HomeWork/HW008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int n = new Random().Next(10, 99); // 1 2 3 4 ... 98
+Console.WriteLine("Случайное число число: " + n);
+
+int x = n%10;
+Console.WriteLine(x);
+int y = n/10;
+Console.WriteLine(y);
+int max = x;
+if ( x > y ) max = x;
+if ( y > x ) max = y;
+Console.Write("Max: " + max);
