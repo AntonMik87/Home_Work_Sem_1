eecfc6e626de0b9bc8f3102c0b6dc8821190f37e
«Anton» <hranitel-liks@mail.ru>
2022-08-08 00:35:29 +0700

    add HW009

---
+++ jobs/HomeWork/HW009/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int n;
+Console.WriteLine("введите трехзначное число: ");
+n = int.Parse(Console.ReadLine());
+int x = n;
+x = (x/10)-(x%100/10)+x%10;
+
+Console.Write("Удалено второе число: " + x);
+
+
