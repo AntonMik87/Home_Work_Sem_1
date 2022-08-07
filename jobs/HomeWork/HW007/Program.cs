d7bfbdaa4e6d9abfc240edf79ff2ef537baa5011
«Anton» <hranitel-liks@mail.ru>
2022-08-07 19:58:47 +0700

    add HW007

---
+++ jobs/HomeWork/HW007/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+int n;
+Console.WriteLine("введите трехзначное число: ");
+n = int.Parse(Console.ReadLine());
+int x = n/10;
+x = x%10;
+
+Console.Write("вторая цифра: " + x);
