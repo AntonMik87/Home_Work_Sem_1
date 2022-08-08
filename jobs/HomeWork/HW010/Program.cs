740f58758287013bd1a38ad0d58ac09a74aa4a02
Anton <hranitel-liks@mail.ru>
2022-08-08 12:15:12 +0700

    add HW010

---
+++ jobs/HomeWork/HW010/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("введите числа: ");
+int x = int.Parse(Console.ReadLine());Console.WriteLine("Делимое: " + x);
+ 
+int y = int.Parse(Console.ReadLine());Console.WriteLine("Делитель: " + y);
+ 
+//x; Делимое
+//y; Делитель
+int z;
+if (x % y == 0) 
+{
+     Console.Write("Кратно");
+}
+ 
+else
+{
+     z = x%y;
+     Console.WriteLine("остаток: " + z);
+}
